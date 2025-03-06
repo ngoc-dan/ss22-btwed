2344f0099190e8fa0ad7de500f2b990fd41920c1
ngoc-dan <ngocdan191026@gmail.com>
2025-03-06 17:15:27 +0700

    Update ss22-bai7.js

--- ss22-bai7.js
+++ ss22-bai7.js
@@ -1,3 +1,11 @@
-let arr = [3,4,6,2,3,9,1,8,7,5]
-arr.sort()
+let arr = [3, 4, 6, 2, 3, 9, 1, 8, 7, 5]
+for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+        if (arr[j] > arr[j + 1]) {
+            let temp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
+        }
+    }
+}
 console.log(arr)
